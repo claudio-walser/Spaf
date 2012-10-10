05720b9fe4ad99f75cde8797c086f36f068d477d
Claudio Walser <claudio@dev.ch>
2012-10-10 01:00:23 +0000

    Add classes in tools as well for documentation

--- _tools/beautify.php
+++ _tools/beautify.php
@@ -20,45 +20,57 @@ namespace Spaf\_tools;
  * @package Spaf\_tools
  * @namespace Spaf\_tools
  */
+final class Beautify {
 
-// hack server argv for windows console
-if (!isset($_SERVER['argv']) && !empty($_GET)) {
-	$_SERVER['argv'] = array('scriptname');
+	/**
+	 * Run the beautify tool
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		// hack server argv for windows console
+		if (!isset($_SERVER['argv']) && !empty($_GET)) {
+			$_SERVER['argv'] = array('scriptname');
 
-	foreach ($_GET as $key => $value) {
-		$_SERVER['argv'][] = $key;
+			foreach ($_GET as $key => $value) {
+				$_SERVER['argv'][] = $key;
+			}
+		}
+
+		// error reporting on
+		error_reporting(E_ALL);
+
+		// chdir to root _tools direcotry in any case
+		chdir(__DIR__);
+
+		// requiere test-autoloader environment
+		require_once('autoloader.php');
+		$loader = new \Spaf\_tools\Autoloader(false);
+
+		// instantiate manager
+		$beautifier = new \Spaf\Library\Php\Beautifier();
+		$directory = new \Spaf\Library\Directory\Directory('../');
+		$files = $directory->getChildren('*.php', 'file', true);
+
+		$beautifier->setFiles($files);
+		$beautifier->beautify();
+
+		foreach ($files as $file) {
+			// path of the original file
+			$orig = $file->getPath() . $file->getName();
+			// new path
+			$new = '../../SpafDuplicate/' . substr($orig, 3, strlen($orig));
+			// write the file to the new path
+			try {
+				$file->write($orig);
+			} catch (\Spaf\Library\Directory\Exception $e) {
+				// thrown if try to save an empty file, we have some int our _tests/Data folder
+			}
+		}
 	}
 }
 
-// error reporting on
-error_reporting(E_ALL);
-
-// chdir to root _tools direcotry in any case
-chdir(__DIR__);
-
-// requiere test-autoloader environment
-require_once('autoloader.php');
-$loader = new \Spaf\_tools\Autoloader(false);
-
-// instantiate manager
-$beautifier = new \Spaf\Library\Php\Beautifier();
-$directory = new \Spaf\Library\Directory\Directory('../');
-$files = $directory->getChildren('*.php', 'file', true);
-
-$beautifier->setFiles($files);
-$beautifier->beautify();
-
-foreach ($files as $file) {
-	// path of the original file
-	$orig = $file->getPath() . $file->getName();
-	// new path
-	$new = '../../SpafDuplicate/' . substr($orig, 3, strlen($orig));
-	// write the file to the new path
-	try {
-		$file->write($orig);
-	} catch (\Spaf\Library\Directory\Exception $e) {
-		// thrown if try to save an empty file, we have some int our _tests/Data folder
-	}
-}
+// run
+$beautify = new Beautify();
 
 ?>
--- _tools/test.php
+++ _tools/test.php
@@ -21,46 +21,69 @@ namespace Spaf\_tools;
  * @namespace Spaf\_tools
  */
 
-// hack server argv for windows console
-if (!isset($_SERVER['argv']) && !empty($_GET)) {
-	$_SERVER['argv'] = array('scriptname');
 
-	foreach ($_GET as $key => $value) {
-		$_SERVER['argv'][] = $key;
+/**
+ * \Spaf\_tests\Test
+ *
+ * Autoloader for test environment.
+ *
+ * @author Claudio Walser
+ * @package Spaf\_tools
+ * @namespace Spaf\_tools
+ */
+final class Test {
+
+	/**
+	 * Run the test tool
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		// hack server argv for windows console
+		if (!isset($_SERVER['argv']) && !empty($_GET)) {
+			$_SERVER['argv'] = array('scriptname');
+
+			foreach ($_GET as $key => $value) {
+				$_SERVER['argv'][] = $key;
+			}
+		}
+
+		// error reporting on
+		error_reporting(E_ALL);
+
+		// chdir to root test direcotry in any case
+		chdir(__DIR__);
+
+		// requiere test-autoloader environment
+		require_once('autoloader.php');
+		$loader = new \Spaf\_tools\Autoloader(false);
+
+		// find all test files
+		$directory = new \Spaf\Library\Directory\Directory('../_tests/');
+		$files = $directory->getChildren('*Test.php', 'file', true);
+
+		$tests = array();
+		foreach ($files as $file) {
+			// @TODO Write a class Spaf\Library\Test\File for it maybe
+			$fileName = $file->getPath() . $file->getName();
+			$className = '\\Spaf\\' . str_replace(array('..' . DIRECTORY_SEPARATOR, '.php', DIRECTORY_SEPARATOR), array('', '', '\\'), $fileName);
+			$tests[] = array(
+				'file' => $fileName,
+				'class' => $className
+			);
+		}
+
+
+		// instantiate manager
+		$manager = new \Spaf\Library\Test\Cli();
+		// set array with tests
+		$manager->setTests($tests);
+		// run test manager
+		$manager->run();
 	}
 }
 
-// error reporting on
-error_reporting(E_ALL);
-
-// chdir to root test direcotry in any case
-chdir(__DIR__);
-
-// requiere test-autoloader environment
-require_once('autoloader.php');
-$loader = new \Spaf\_tools\Autoloader(false);
-
-// find all test files
-$directory = new \Spaf\Library\Directory\Directory('../_tests/');
-$files = $directory->getChildren('*Test.php', 'file', true);
-
-$tests = array();
-foreach ($files as $file) {
-	// @TODO Write a class Spaf\Library\Test\File for it maybe
-	$fileName = $file->getPath() . $file->getName();
-	$className = '\\Spaf\\' . str_replace(array('..' . DIRECTORY_SEPARATOR, '.php', DIRECTORY_SEPARATOR), array('', '', '\\'), $fileName);
-	$tests[] = array(
-		'file' => $fileName,
-		'class' => $className
-	);
-}
-
-
-// instantiate manager
-$manager = new \Spaf\Library\Test\Cli();
-// set array with tests
-$manager->setTests($tests);
-// run test manager
-$manager->run();
+// run
+$test = new Test();
 
 ?>
