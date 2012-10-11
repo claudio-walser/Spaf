3547de6f599eb24329bfb1ab60341abba0bfaac5
Claudio Walser <claudio@dev.ch>
2012-10-11 20:25:00 +0000

    Documentation fixes

--- _tests/Unit/Core/Controller/NotFoundTest.php
+++ _tests/Unit/Core/Controller/NotFoundTest.php
@@ -3,7 +3,7 @@
 /**
  * $Id$
  *
- * Spaf/_tests/Core/Controller/IndexTest.php
+ * Spaf/_tests/Unit/Core/Controller/IndexTest.php
  * @created Wed Oct 3 19:56:13 CET 2012
  * @author Claudio Walser
  * @reviewer TODO
@@ -11,13 +11,13 @@
 namespace Spaf\_tests\Unit\Core\Controller;
 
 /**
- * \Spaf\_tests\Core\Controller\IndexTest
+ * \Spaf\_tests\Unit\Core\Controller\IndexTest
  *
  * The DispatcherTest class tests any aspect of \Spaf\Core\Controller\Index.
  *
  * @author Claudio Walser
- * @package Spaf\_tests\Core\Controller
- * @namespace Spaf\_tests\Core\Controller
+ * @package Spaf\_tests\Unit\Core\Controller
+ * @namespace Spaf\_tests\Unit\Core\Controller
  */
 class NotFoundTest extends \PHPUnit_Framework_TestCase {
 
