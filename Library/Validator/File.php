b8a69e7f6b4e5cedc391b596fa7177ff60ed5296
Claudio Walser <claudio@dev.ch>
2012-10-12 21:49:05 +0000

    First thoughts of validator classes, this time in the right branch :p

---
+++ Library/Validator/File.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * $ID$
+ *
+ * Spaf/Library/Validator/File.php
+ * @created Wed Oct 10 21:11:04 +0200 2012
+ * @author Claudio Walser
+ * @reviewer TODO
+ */
+
+namespace Spaf\Library\Validator;
+
+
+/**
+ * \Spaf\Library\Validator\Abstraction
+ *
+ * File validation class
+ *
+ * @author Claudio Walser
+ * @package Spaf\Library\Validator
+ * @namespace Spaf\Library\Validator
+ */
+
+class File extends Abstraction {}
+
+?>
