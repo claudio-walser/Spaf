6423210433cda00d5ef0f6a726a1e8901e37c75e
claudio.walser <claudio@dev.local>
2012-11-16 08:32:34 +0100

    Changed my mind for authority

---
+++ Library/Authority/Control.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * $Id$
+ *
+ * Spaf/Library/Authority/Control.php
+ * @created Sat Sep 09 09:35:01 CET 2006
+ * @author Claudio Walser
+ * @reviewer TODO
+ */
+namespace Spaf\Library\Authority;
+
+/**
+ * \Spaf\Library\Authority\Control
+ *
+ * @author Claudio Walser
+ * @package Spaf\Library\Authority
+ * @namespace Spaf\Library\Authority
+ */
+class Control {
+    
+    
+    
+}
+?>
