3eb9fd680e7d7487c16e845df5a645f598dc024d
root <root@web-01.development.claudio.dev>
2014-10-16 22:36:50 +0200

    Adapted test autoloader to new Autoloader class

--- tools/autoloader.php
+++ tools/autoloader.php
@@ -24,62 +24,16 @@ require_once('../src/Spaf/Core/Autoloader.php');
  */
 final class Autoloader Extends \Spaf\Core\Autoloader {
 
-	/**
-	 * Load the only needed phpunit file by hand.
-	 * Then call parent, which is loading any of our functions below
-	 * as spl_autoload and passing $name as first and only param.
-	 */
+	protected $_lookupPaths = array(
+		'../src/',
+		'../../'
+	);
+
 	public function __construct() {
 		// phpunit procedural autoload functions
 		require_once('PHPUnit/Autoload.php');
 
 		parent::__construct();
-	}
-
-	/**
-	 * Autoloader for the Spaf Framework classes
-	 *
-	 * @param string Classname with complete namespace
-	 * @return boolean
-	 */
-	public function Spaf($name) {
-		$file = $this->_getFilename($name);
-		$file = '../src/' . $file;
-
-		$this->debug($file . "<br />\n");
-
-		if (is_file($file) && is_readable($file)) {
-			// do debug message if needed
-			$this->debug('<strong>' . date('Y-m-d H:i:s') . '</strong> -- File ' . $file . ' successfully loaded' . "<br />\n");
-			// require the file
-			require_once($file);
-			return true;
-		} else {
-			$this->debug('<strong>' . date('Y-m-d H:i:s') . '</strong> -- File ' . $file . ' not found; include_path:' . get_include_path() . "<br />\n");
-			return false;
-		}
-	}
-
-	/**
-	 * Autoloader for test classes
-	 *
-	 * @param String Classname with complete namespace
-	 * @return boolean
-	 */
-	public function tests($name) {
-		$file = '../../' . $this->_getFilename($name);
-		$this->debug($file . "<br />\n");
-
-		if (is_file($file) && is_readable($file)) {
-			// do debug message if needed
-			$this->debug('<strong>' . date('Y-m-d H:i:s') . '</strong> -- File ' . $file . ' successfully loaded' . "<br />\n");
-			// require the file
-			require_once($file);
-			return true;
-		} else {
-			$this->debug('<strong>' . date('Y-m-d H:i:s') . '</strong> -- File ' . $file . ' not found; include_path:' . get_include_path() . "<br />\n");
-			return false;
-		}
 	}
 
 }
