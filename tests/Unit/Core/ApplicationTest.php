6ddcf7d79e5a82055b0b601665511b4507b6c73d
Claudio Walser <claudio@claudio.dev>
2014-10-18 08:44:22 +0200

    Changed tests as well

--- tests/Unit/Core/ApplicationTest.php
+++ tests/Unit/Core/ApplicationTest.php
@@ -85,57 +85,7 @@ class ApplicationTest extends \Spaf\tests\Unit\TestCase {
 			$this->_application->getRegistry()
 		);
 
-
-
-		// is this functionality really needed? Shouldnt it just always update
-		// its dispatcher as well?
-		// Updating the registry without updating the dispatcher
-		// might just cause some errors. Is it usefull for anything?
-
-		// put another registry again, but do not update the dispatcher
-		$this->_application->setRegistry($origRegistry, false);
-		$this->assertNotEquals(
-			$dispatcher->getRegistry(),
-			$this->_application->getRegistry()
-		);
-
-		$this->_application->setRegistry($origRegistry, true);
-		$this->assertEquals(
-			$dispatcher->getRegistry(),
-			$this->_application->getRegistry()
-		);
-
 		unset($registry);
-	}
-
-	/**
-	 * Test setDispatcher and getDispatcher.
-	 * Means test it by normal registry object and test
-	 * mock-object injection as well.
-	 *
-	 * @return void
-	 */
-	public function testSetGetDispatcher() {
-		// default should be \Spaf\Core\Dispatcher, check on that first
-		$dispatcher = new \Spaf\Core\Dispatcher();
-		// objects arent the same, since we instantiate a new Dispatcher above
-		// but its okay if the are from the same class
-		$this->assertEquals(
-			get_class($dispatcher),
-			get_class($this->_application->getDispatcher())
-		);
-
-
-		// now inject a mocked dispatcher
-		$dispatcher = new \Spaf\tests\Mock\Core\Dispatcher();
-		$this->_application->setDispatcher($dispatcher);
-
-		$this->assertEquals(
-			$dispatcher,
-			$this->_application->getDispatcher()
-		);
-
-		unset($dispatcher);
 	}
 
 	/**
@@ -166,28 +116,7 @@ class ApplicationTest extends \Spaf\tests\Unit\TestCase {
 			$this->_response,
 			$this->_application->getResponse()
 		);
-
 	}
-
-	/**
-	 * Test setRequest and getRequest.
-	 * Means test it by normal request object.
-	 *
-	 * @todo Implement with mock objects not sending any headers
-	 * @return void
-	  */
-	/*public function testSetGetResponse() {
-		$response = new \Spaf\Core\Response\Json();
-
-		$this->_application->setResponse($response);
-
-		$this->assertEquals(
-			$response,
-			$this->_application->getResponse()
-		);
-
-	}
-	*/
 
 	/**
 	 * Release some memory maybe
